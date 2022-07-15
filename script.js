d683e7e1f032460792705c045c521e357718c179
jrjatin <70600563+jrjatin@users.noreply.github.com>
2022-07-15 16:53:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('.clicks').on('click',function(){
+        $('#box').addClass('show');
+    })
+
+    $('.submission').on('click',function(){
+        $('#box').removeClass('show');
+    })
+    $('.icon').on('click',function(){
+        $('#box').removeClass('show');
+    })
+});
